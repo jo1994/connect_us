413be7fdfc7000d5b777bb6696b248cc0b4f9e39
Roberto Villalta <roberto15394@gmail.com>
2024-07-19 20:24:45 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+let localStream;
+let remoteStream;
+
+let init = async () => {
+localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+document.getElementById('user-1').srcObject = localStream
+}
+
+let createOffer = async() => {
+    peerConnection = new RTCPeerConnection()
+    remoteStream = new MediaStream()
+    document.getElementById('user-2').srcObject = remoteStream
+    let offer = await peerConnection.createOffer()
+    await peerConnection.setLocalDescription(offer)
+
+    console.log('Offer:', offer)
+}
+
+init()
+
